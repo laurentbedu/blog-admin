fb47d3aed27f24733f67a003f4a570f6c26fde5f
laurentbedu <bedulaurent@gmail.com>
2022-09-19 15:01:59 +0200

    TP-10

--- src/screens/AccountDetailScreen.jsx
+++ src/screens/AccountDetailScreen.jsx
@@ -7,16 +7,22 @@ function AccountDetailScreen() {
     const [user, setUser] = useState(null);
 
     useEffect(() => {
-        fetch("http://blog.api/appuser/"+id)
+        fetch("http://blog.api/appuser/"+id, {
+                method: "POST",
+                body: JSON.stringify({with:['account','role']})
+            })
             .then(resp => resp.json())
             .then(json => {
                 setUser(json)
             });
+        
 
     }, [id]);
 
     return ( <>
         <h1>Détail de l'utilisateur : {user?.pseudo}</h1>
+        <b>email :</b> {user?.account?.login}<br/>
+        <b>role :</b> {user?.role?.title}
     </> );
 }
 
--- src/screens/AccountScreen.jsx
+++ src/screens/AccountScreen.jsx
@@ -5,14 +5,14 @@ function AccountScreen() {
     const [users, setUsers] = useState([]);
 
     useEffect(() => {
-        fetch("http://blog.api/appuser")
-            .then(resp => resp.json())
-            .then(json => {
-                json = json.sort((a,b) => {
-                    return a.pseudo.toLowerCase() > b.pseudo.toLowerCase() ? 1 : -1
-                });
-                setUsers(json)
-            });
+        fetch("http://blog.api/appuser/0", {
+            method: "POST",
+            body: JSON.stringify({with:['account','role']})
+        })
+        .then(resp => resp.json())
+        .then(json => {
+            setUsers(json)
+        });
 
     }, [])
 
@@ -24,6 +24,8 @@ function AccountScreen() {
             <thead>
                 <tr>
                     <th scope="col">pseudo</th>
+                    <th scope="col">email</th>
+                    <th scope="col">role</th>
                     <th scope="col">actions</th>
                 </tr>
             </thead>
@@ -31,6 +33,8 @@ function AccountScreen() {
                 {users.map(user => {
                     return(<tr key={user.Id_appUser} onClick={()=>{navigate(`/account/${user.Id_appUser}`);}}>
                         <td>{user.pseudo}</td>
+                        <td>{user.account?.login}</td>
+                        <td>{user.role?.title}</td>
                         <td></td>
                     </tr>);
                 })}
